eb60153d3afc118998e4dfd36487ab689463bf33
arash-mehrzadi <mehrzadiarash@gmail.com>
2022-08-15 12:37:23 -0700

    MOE_M.py - mixture of expert model compiled and train successfully

--- src/MOE_M.py
+++ src/MOE_M.py
@@ -1,50 +1,39 @@
-# import os
-# from numpy import genfromtxt
+import os
+from numpy import genfromtxt
 import numpy as np
-# import matplotlib.pyplot as plt
-# import sklearn
-# import tensorflow as tf
-# from tensorflow import keras
-# from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+import sklearn
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 import tensorflow_probability as tfp
-# from sklearn.model_selection import train_test_split
-# import pandas as pd
+from sklearn.metrics import accuracy_score
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier # RF
-from sklearn.ensemble import ExtraTreesClassifier # ET
-# from sklearn.ensemble import AdaBoostClassifier # ADB
-# from sklearn.svm import SVC # svc
-from sklearn.neural_network import MLPClassifier # MLP
-# from xgboost import XGBClassifier # XGB
-# from sklearn.gaussian_process import GaussianProcessClassifier # GPC
-# from sklearn.naive_bayes import GaussianNB # GNB
-from sklearn.linear_model import LogisticRegression # LR
-# from sklearn.ensemble import GradientBoostingClassifier # GBC
-# from sklearn.metrics import accuracy_score
-# from sklearn.metrics import f1_score
-# from sklearn.metrics import precision_score
-# from sklearn.metrics import recall_score
-# import scipy
-# from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier  # RF
+from sklearn.ensemble import ExtraTreesClassifier  # ET
+from sklearn.neural_network import MLPClassifier  # MLP
+from sklearn.linear_model import LogisticRegression  # LR
 from sklearn.model_selection import KFold
-# import statistics
-# from tensorflow.keras.optimizers import SGD
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras import backend as K
-# from tensorflow.keras.layers import Input
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.preprocessing import MinMaxScaler
-# from tensorflow.keras.layers.experimental import preprocessing
-# from tensorflow.keras.layers import Dropout, BatchNormalization
+import statistics
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Input
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.layers.experimental import preprocessing
+from tensorflow.keras.layers import Dropout, BatchNormalization
+
 np.set_printoptions(precision=3, suppress=True)
 
 tfd = tfp.distributions
 
+
 class moe:
     def __init__(self, parameters, HP, dataset_name):
         self.datapath = parameters[dataset_name]
-        self.resname= dataset_name
+        self.resname = dataset_name
         self.test_size = parameters['test_size']
         self.label = parameters['label']
         self.random_state = parameters['random_state']
@@ -66,12 +55,12 @@ class moe:
         self.model_LR = LogisticRegression(solver='newton-cg',
                                            C=100)
         self.Data_Prepration()
-        #self.results_dataframe = pd.DataFrame(columns=['method', 'Accuracy', 'sensitivity', 'specificity', 'f1'])
+        # self.results_dataframe = pd.DataFrame(columns=['method', 'Accuracy', 'sensitivity', 'specificity', 'f1'])
         self.experience_path = HP
-        self.moe_Inputs= {}
+        self.moe_Inputs = {}
 
     def Data_Prepration(self):
-        df_input= pd.read_excel(self.datapath)
+        df_input = pd.read_excel(self.datapath)
         Y = df_input.pop(self.label)
         X = df_input
         X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=self.test_size,
@@ -119,8 +108,7 @@ class moe:
                 self.report(i)
                 self.moe_Inputs[i] = self.Res_LR
 
-
-    def predict_REs(self, X_test, X_train,  model):
+    def predict_REs(self, X_test, X_train, model):
 
         train_res = model.predict_proba(X_train)
         test_res = model.predict_proba(X_test)
@@ -130,96 +118,155 @@ class moe:
     def report(self, name):
         print("Module - " + name + " Done!")
 
+    def prior_mean_field(self, kernel_size, bias_size, dtype=None):  # prior Function
+        n = kernel_size + bias_size
+        return lambda t: tfd.Independent(tfd.Normal(loc=tf.zeros(n, dtype=dtype),
+                                                    scale=tf.ones(n)),
+                                         reinterpreted_batch_ndims=1)
 
+    def posterior_mean_field(self, kernel_size, bias_size=0, dtype=None):  # Posterior Function
+        n = kernel_size + bias_size
+        c = np.log(np.expm1(1.))
+        return tf.keras.Sequential([
+            tfp.layers.VariableLayer(2 * n, dtype=dtype),
+            tfp.layers.DistributionLambda(lambda t: tfd.Independent(
+                tfd.Normal(loc=t[..., :n],
+                           scale=1e-5 + 0.01 * tf.nn.softplus(c + t[..., n:])),
+                reinterpreted_batch_ndims=1)),
+        ])
 
+    def prior(self, kernel_size, bias_size, dtype=None):
+        n = kernel_size + bias_size
+        prior_model = keras.Sequential(
+            [
+                tfp.layers.DistributionLambda(
+                    lambda t: tfp.distributions.MultivariateNormalDiag(
+                        loc=tf.zeros(n), scale_diag=tf.ones(n)
+                    )
+                )
+            ]
+        )
+        return prior_model
 
+    def posterior(self, kernel_size, bias_size, dtype=None):
+        n = kernel_size + bias_size
+        posterior_model = keras.Sequential(
+            [
+                tfp.layers.VariableLayer(
+                    tfp.layers.MultivariateNormalTriL.params_size(n), dtype=dtype
+                ),
+                tfp.layers.MultivariateNormalTriL(n),
+            ]
+        )
+        return posterior_model
 
+    def priorED1(self, kernel_size, bias_size, dtype=None):
 
+        n = kernel_size + bias_size
 
+        prior_model = Sequential([tfp.layers.DistributionLambda(
+            lambda t: tfd.MultivariateNormalDiag(loc=tf.zeros(n), scale_diag=tf.ones(n)))])
 
+        return (prior_model)
 
+    def posteriorED1(self, kernel_size, bias_size, dtype=None):
 
+        n = kernel_size + bias_size
 
+        posterior_model = Sequential([
 
+            tfp.layers.VariableLayer(tfp.layers.MultivariateNormalTriL.params_size(n), dtype=dtype),
+            tfp.layers.MultivariateNormalTriL(n)])
 
+        return (posterior_model)
 
+    # def negative_loglikelihood(self, targets, estimated_distribution):
+    #     return -estimated_distribution.log_prob(targets)
+    # noise = 1.0
+    # def negative_loglikelihood(self, y_obs, y_pred, sigma=noise):
+    #    dist = tfp.distributions.Normal(loc=y_pred, scale=sigma)
+    #    return keras.backend.sum(-dist.log_prob(tf.cast(y_obs, tf.float32)))
+    def negative_loglikelihood(self, targets, estimated_distribution):
+        return -estimated_distribution.log_prob(tf.cast(targets, tf.float32))
 
+    def Model(self):
+        self.models_Prepration()
+        hidden_units = [8, 8]
+        batch_size = 50
+        counter_L = 0
+        model = Sequential()
+        InData_Ex1 = Input(shape=(self.moe_Inputs['RF'][0].shape[1],), name="Input_Ex1")
+        InData_Ex2 = Input(shape=(self.moe_Inputs['ET'][0].shape[1],), name="Input_EX2")
+        InData_Ex3 = Input(shape=(self.moe_Inputs['MLP'][0].shape[1],), name="Input_EX3")
+        InData_Ex4 = Input(shape=(self.moe_Inputs['LR'][0].shape[1],), name="Input_EX4")
+        # features = layers.Concatenate([InData_Ex1, InData_Ex2, InData_Ex3, InData_Ex4])
+        InData = layers.Concatenate(axis=-1)([InData_Ex1, InData_Ex2, InData_Ex3, InData_Ex4])
+        InData = BatchNormalization()(InData)
+        features = InData
+        # features = layers.Dense(units=8, activation='relu')(features)
+        # features = layers.Dense(units=16, activation='relu')(features)
+        # features = layers.Dense(units=32, activation='relu')(features)
+        for units in hidden_units:
+            features = tfp.layers.DenseVariational(
+                units=units,
+                make_prior_fn=self.prior_mean_field,
+                make_posterior_fn=self.posterior_mean_field,
+                kl_weight=1 / self.moe_Inputs['LR'][0].shape[0],
+                activation='sigmoid',
+            )(features)
+        outputs = layers.Dense(units=1)(features)
+        # distribution_params = layers.Dense(units=2)(features)
+        # outputs = tfp.layers.IndependentNormal(1)(distribution_params)
 
+        model = keras.Model(inputs=[InData_Ex1, InData_Ex2, InData_Ex3, InData_Ex4], outputs=outputs)
+        model.compile(
+            optimizer=keras.optimizers.RMSprop(learning_rate=0.001),
+            loss="binary_crossentropy",
+            metrics=['accuracy'])
+        print(model.summary())
+        # history = model.fit([self.moe_Inputs['RF'][0],
+        #                     self.moe_Inputs['ET'][0],
+        #                     self.moe_Inputs['MLP'][0],
+        #                     self.moe_Inputs['LR'][0]],
+        #                     self.y_train,
+        #                     epochs=150,
+        #                     validation_data=([self.moe_Inputs['RF'][1],
+        #                                       self.moe_Inputs['ET'][1],
+        #                                       self.moe_Inputs['MLP'][1],
+        #                                       self.moe_Inputs['LR'][1]],
+        #                                       self.y_test),
+        #                                       batch_size=64)
+        self.moe_model = model
 
+    def MOE_Run(self):
+        self.Model()
+        moe_acc = []
+        history = self.moe_model.fit([self.moe_Inputs['RF'][0],
+                                      self.moe_Inputs['ET'][0],
+                                      self.moe_Inputs['MLP'][0],
+                                      self.moe_Inputs['LR'][0]],
+                                     self.y_train,
+                                     epochs=1000,
+                                     validation_data=([self.moe_Inputs['RF'][1],
+                                                       self.moe_Inputs['ET'][1],
+                                                       self.moe_Inputs['MLP'][1],
+                                                       self.moe_Inputs['LR'][1]],
+                                                      self.y_test),
+                                     batch_size=8)
+        predicted = self.moe_model.predict([self.moe_Inputs['RF'][1],
+                                            self.moe_Inputs['ET'][1],
+                                            self.moe_Inputs['MLP'][1],
+                                            self.moe_Inputs['LR'][1]])
+        print(predicted)
+        # for i in range(100):
+        #   predicted= self.moe_model.predict([self.moe_Inputs['RF'][1],
+        #                                      self.moe_Inputs['ET'][1],
+        #                                      self.moe_Inputs['MLP'][1],
+        #                                      self.moe_Inputs['LR'][1]])
+        #   moe_acc.append(accuracy_score(self.y_test, predicted))
+        #   print(moe_acc.mean(), moe_acc.std())
+        return history
 
-
-
-
-
-# def prior_mean_field(kernel_size, bias_size, dtype=None):   #prior Function
-#     n = kernel_size + bias_size
-#     return lambda t: tfd.Independent(tfd.Normal(loc=tf.zeros(n, dtype=dtype),
-#                                                 scale=tf.ones(n)),
-#                                      reinterpreted_batch_ndims=1)
-#
-# def posterior_mean_field(kernel_size, bias_size=0, dtype=None):   #Posterior Function
-#   n = kernel_size + bias_size
-#   c = np.log(np.expm1(1.))
-#   return tf.keras.Sequential([
-#       tfp.layers.VariableLayer(2 * n, dtype=dtype),
-#       tfp.layers.DistributionLambda(lambda t: tfd.Independent(
-#           tfd.Normal(loc=t[..., :n],
-#                      scale=1e-5 + 0.01*tf.nn.softplus(c + t[..., n:])),
-#           reinterpreted_batch_ndims=1)),
-#   ])
-#
-#
-#
-# def Model(Data_Path, y):  # Prepair Model
-#     try:
-#         Data = pd.read_csv(Data_Path)
-#     except:
-#         Data = pd.read_excel(Data_Path)
-#
-#     X = Data
-#     Y = y
-#     columns_name = X.columns
-#     ssc = StandardScaler()
-#     ssc.fit(X)
-#     X = ssc.transform(X)
-#     scaler = MinMaxScaler()
-#     scaler.fit(X)
-#     normalized = scaler.transform(X)
-#     X_train1, X_test1, Y_train1, Y_test1 = train_test_split(normalized, y, test_size=0.2, random_state=100)
-#
-#
-#
-#     hidden_units = [2, 2, 2]
-#     dataset_size = len(Data)
-#     batch_size = 50
-#     counter_L = 0
-#     model = Sequential()
-#     InData_Ex1 = Input(shape=(X.shape[1],), name="Input_Ex1")
-#     InData_Ex2 = Input(shape=(X.shape[1],), name="Input_EX2")
-#     InData_Ex3 = Input(shape=(X.shape[1],), name="Input_EX3")
-#     InData_Ex4 = Input(shape=(X.shape[1],), name="Input_EX4")
-#     #features = layers.Concatenate([InData_Ex1, InData_Ex2, InData_Ex3, InData_Ex4])
-#     InData= layers.Concatenate(axis=-1)([InData_Ex1, InData_Ex2, InData_Ex3, InData_Ex4])
-#     InData = BatchNormalization()(InData)
-#     features= InData
-#     for units in hidden_units:
-#         features = tfp.layers.DenseVariational(
-#             units=units,
-#             make_prior_fn=prior_mean_field,
-#             make_posterior_fn=posterior_mean_field,
-#             kl_weight=1 / X.shape[0],
-#             activation='relu',
-#         )(features)
-#     features = layers.Dense(units=1, activation='sigmoid')(features)
-#     model = keras.Model(inputs=[InData_Ex1, InData_Ex2, InData_Ex3, InData_Ex4], outputs=features)
-#     model.compile(
-#         optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-#         loss='binary_crossentropy',
-#         metrics=['accuracy'],
-#     )
-#     #history = model.fit(X_train1, Y_train1, validation_data=(X_test1, Y_test1), epochs=500)
-#
-#     return model, [X_test1, Y_test1], [X_train1, Y_train1]
 
 
 
